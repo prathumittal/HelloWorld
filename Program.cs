8e06303ed390ab822e804d2c5698a932d19c4faf
Puneet Mittal <puneet.mittal@nextgearsolutions.com>
2020-11-26 19:52:05 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Next Gear Team");
+            Console.Read();
+        }
+    }
+}
